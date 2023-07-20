063f51595044f30d5c7b0722a62458bfb640b6bd
insanz01 <insankamil002@gmail.com>
2023-07-20 15:18:09 +0700

    add script for build and git ignore

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Jalankan perintah go test
+echo "Running go test..."
+go test ./test
+
+# Periksa apakah perintah go test selesai dengan sukses (exit code 0)
+if [ $? -eq 0 ]; then
+  # Jika go test berhasil, jalankan perintah go build
+  echo "Running go build..."
+  go build -o app
+  echo "Build successful! Executable binary created as app"
+else
+  # Jika go test gagal, tampilkan pesan kesalahan dan hentikan build
+  echo "Unit tests failed! Build aborted."
+fi
